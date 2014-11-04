71ac01ba5fc0042bb1b3588039a28d843fb5ed0e
Laurent Declercq <l.declercq@nuxwin.com>
2014-11-04 08:49:08 +0100

    Added imscp configuration template file

--- config/imscp.config.inc.php
+++ config/imscp.config.inc.php
@@ -2,11 +2,10 @@
 
 /*
  +-----------------------------------------------------------------------+
- | Local configuration for the Roundcube Webmail installation.           |
+ | Local configuration for the i-MSCP Webmail installation.              |
  |                                                                       |
- | This is a sample configuration file only containing the minumum       |
- | setup required for a functional installation. Copy more options       |
- | from defaults.inc.php to this file to override the defaults.          |
+ | Copy more options from defaults.inc.php to this file to               |
+ | override the defaults.                                                |
  |                                                                       |
  | This file is part of the Roundcube Webmail client                     |
  | Copyright (C) 2005-2013, The Roundcube Dev Team                       |
@@ -24,7 +23,11 @@ $config = array();
 // Currently supported db_providers: mysql, pgsql, sqlite, mssql or sqlsrv
 // For examples see http://pear.php.net/manual/en/package.database.mdb2.intro-dsn.php
 // NOTE: for SQLite use absolute path: 'sqlite:////full/path/to/sqlite.db?mode=0646'
-$config['db_dsnw'] = 'mysql://roundcube:pass@localhost/roundcubemail';
+$config['db_dsnw'] = 'mysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}';
+
+// ----------------------------------
+// IMAP
+// ----------------------------------
 
 // The mail host chosen to perform the log-in.
 // Leave blank to show a textbox at login, give a list of hosts
@@ -36,7 +39,20 @@ $config['db_dsnw'] = 'mysql://roundcube:pass@localhost/roundcubemail';
 // %d - domain (http hostname $_SERVER['HTTP_HOST'] without the first part)
 // %s - domain name after the '@' from e-mail address provided at login screen
 // For example %n = mail.domain.tld, %t = domain.tld
-$config['default_host'] = 'localhost';
+// WARNING: After hostname change update of mail_host column in users table is
+//          required to match old user data records with the new host.
+$config['default_host'] = 'imap.{BASE_SERVER_VHOST}';
+
+// By default list of subscribed folders is determined using LIST-EXTENDED
+// extension if available. Some servers (dovecot 1.x) returns wrong results
+// for shared namespaces in this case. http://trac.roundcube.net/ticket/1486225
+// Enable this option to force LSUB command usage instead.
+// Deprecated: Use imap_disabled_caps = array('LIST-EXTENDED')
+$config['imap_force_lsub'] = true;
+
+// ----------------------------------
+// SMTP
+// ----------------------------------
 
 // SMTP server host (for sending mails).
 // To use SSL/TLS connection, enter hostname with prefix ssl:// or tls://
@@ -48,38 +64,64 @@ $config['default_host'] = 'localhost';
 // %d - domain (http hostname $_SERVER['HTTP_HOST'] without the first part)
 // %z - IMAP domain (IMAP hostname without the first part)
 // For example %n = mail.domain.tld, %t = domain.tld
-$config['smtp_server'] = '';
-
-// SMTP port (default is 25; use 587 for STARTTLS or 465 for the
-// deprecated SSL over SMTP (aka SMTPS))
-$config['smtp_port'] = 25;
+#$config['smtp_server'] = 'smtp.{BASE_SERVER_VHOST}';
+$config['smtp_server'] = 'smtp.{BASE_SERVER_VHOST}';
 
 // SMTP username (if required) if you use %u as the username Roundcube
 // will use the current username for login
-$config['smtp_user'] = '';
+$config['smtp_user'] = '%u';
 
 // SMTP password (if required) if you use %p as the password Roundcube
 // will use the current user's password for login
-$config['smtp_pass'] = '';
+$config['smtp_pass'] = '%p';
 
-// provide an URL where a user can get support for this Roundcube installation
-// PLEASE DO NOT LINK TO THE ROUNDCUBE.NET WEBSITE HERE!
-$config['support_url'] = '';
+// ----------------------------------
+// SYSTEM
+// ----------------------------------
 
-// Name your service. This is displayed on the login screen and in the window title
-$config['product_name'] = 'Roundcube Webmail';
+// use this folder to store temp files (must be writable for apache user)
+$config['temp_dir'] = '{TMP_PATH}';
+
+// Session lifetime in minutes
+$config['session_lifetime'] = 20;
 
 // this key is used to encrypt the users imap password which is stored
 // in the session record (and the client cookie if remember password is enabled).
 // please provide a string of exactly 24 chars.
-// YOUR KEY MUST BE DIFFERENT THAN THE SAMPLE VALUE FOR SECURITY REASONS
-$config['des_key'] = 'rcmail-!24ByteDESkey*Str';
+$config['des_key'] = '{DES_KEY}';
+
+// Name your service. This is displayed on the login screen and in the window title
+$config['product_name'] = 'i-MSCP Webmail';
+
+// Set identities access level:
+// 0 - many identities with possibility to edit all params
+// 1 - many identities with possibility to edit all params but not email address
+// 2 - one identity with possibility to edit all params
+// 3 - one identity with possibility to edit all params but not email address
+// 4 - one identity with possibility to edit only signature
+$config['identities_level'] = 3;
+
+// ----------------------------------
+// PLUGINS
+// ----------------------------------
 
 // List of active plugins (in plugins/ directory)
 $config['plugins'] = array(
-    'archive',
-    'zipdownload',
+	'archive',
+	'zipdownload',
 );
 
-// skin name: folder from skins/
-$config['skin'] = 'larry';
+// ----------------------------------
+// USER INTERFACE
+// ----------------------------------
+
+// automatically create the above listed default folders on first login
+$config['create_default_folders'] = true;
+
+// if in your system 0 quota means no limit set this option to true
+$config['quota_zero_as_unlimited'] = true;
+
+// Make use of the built-in spell checker. It is based on GoogieSpell.
+// Since Google only accepts connections over https your PHP installation
+// requires to be compiled with Open SSL support
+$config['enable_spellcheck'] = false;
